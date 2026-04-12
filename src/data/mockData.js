8aa614d8fef652d212b2b54817d3a6708868ea4b
mmkusf-ctrl <m.m.k.usf@gmail.com>
2026-04-11 23:10:22 -0400

    Create mockData.js

---
+++ src/data/mockData.js
@@ -0,0 +1,61 @@
+export const liveMatches = [
+  {
+    id: 1,
+    team1: 'ICAT',
+    team2: 'Toons CC',
+    score1: '154/4 (16.2)',
+    score2: '148/8 (20)',
+    venue: 'Tampa Cricket Ground',
+    status: 'Live',
+    result: 'ICAT need 5 runs in 22 balls'
+  },
+  {
+    id: 2,
+    team1: 'Falcons CC',
+    team2: 'Floridians CC',
+    score1: 'Starts 6:30 PM',
+    score2: 'League Match',
+    venue: 'Sarasota Oval',
+    status: 'Upcoming',
+    result: 'Playing XI to be announced'
+  },
+  {
+    id: 3,
+    team1: 'Silver Hawks',
+    team2: 'Game Changers',
+    score1: '172/6',
+    score2: '165/9',
+    venue: 'Orlando Turf',
+    status: 'Result',
+    result: 'Silver Hawks won by 7 runs'
+  }
+]
+
+export const pointsTable = [
+  { team: 'Toons CC', p: 8, w: 7, l: 1, pts: 14, nrr: '+1.82' },
+  { team: 'ICAT', p: 8, w: 6, l: 2, pts: 12, nrr: '+1.21' },
+  { team: 'Game Changers', p: 8, w: 5, l: 3, pts: 10, nrr: '+0.74' },
+  { team: 'Falcons CC', p: 8, w: 4, l: 4, pts: 8, nrr: '+0.12' },
+  { team: 'Floridians CC', p: 8, w: 3, l: 5, pts: 6, nrr: '-0.41' }
+]
+
+export const topBatters = [
+  { name: 'Aviral Jain', team: 'Toons CC', stat: '864 Runs' },
+  { name: 'Advait Varadharajan', team: 'Floridians CC', stat: '710 Runs' },
+  { name: 'Nick Panwar', team: 'Falcons CC', stat: '661 Runs' },
+  { name: 'Luvv Sachdeva', team: 'ICAT', stat: '543 Runs' }
+]
+
+export const topBowlers = [
+  { name: 'Moeen Sunasra', team: 'Game Changers', stat: '36 Wkts' },
+  { name: 'Leslie Daniel Raj', team: 'Toons CC', stat: '31 Wkts' },
+  { name: 'Vamshi Krishna Mekala', team: 'Silver Hawks', stat: '31 Wkts' },
+  { name: 'Ali Akbar', team: 'Game Changers', stat: '28 Wkts' }
+]
+
+export const news = [
+  'ICAT announces new season fixtures',
+  'Leslie Daniel Raj named player of the week',
+  'FWCWL points table tightens after weekend matches',
+  'ICAT to introduce advanced score analytics soon'
+]
