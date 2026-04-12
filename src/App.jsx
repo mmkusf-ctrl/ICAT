04dc00990ba6934826fdb556ad90fdc3f111f618
mmkusf-ctrl <m.m.k.usf@gmail.com>
2026-04-11 23:09:51 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import LiveScores from './pages/LiveScores'
+import Schedule from './pages/Schedule'
+import Teams from './pages/Teams'
+import Players from './pages/Players'
+import PointsTablePage from './pages/PointsTablePage'
+import News from './pages/News'
+
+export default function App() {
+  return (
+    <div className="app-shell">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/live" element={<LiveScores />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/teams" element={<Teams />} />
+        <Route path="/players" element={<Players />} />
+        <Route path="/points-table" element={<PointsTablePage />} />
+        <Route path="/news" element={<News />} />
+      </Routes>
+    </div>
+  )
+}
