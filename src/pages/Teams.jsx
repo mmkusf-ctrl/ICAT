7e8a8acc44bc93f2801ff06f9a3d50ccae583881
mmkusf-ctrl <m.m.k.usf@gmail.com>
2026-04-11 23:15:35 -0400

    Create Teams.jsx

---
+++ src/pages/Teams.jsx
@@ -0,0 +1,8 @@
+export default function Teams() {
+  return (
+    <main className="container">
+      <h1 className="section-title">Teams</h1>
+      <div className="card">ICAT, Toons CC, Falcons CC, Floridians CC, and more.</div>
+    </main>
+  )
+}
